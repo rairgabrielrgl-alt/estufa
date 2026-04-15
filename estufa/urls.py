d4c7c8544663aad70980a5ae99a8f4f8c6f1c675
Rair Gabriel <rairgabrielrgl@gmail.com>
2026-04-15 16:27:39 -0300

    Atual2

--- estufa/urls.py
+++ estufa/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import redirect
 
-
+def home(request):
+    return redirect('/monitoramento/painel/')
 
 urlpatterns = [
     path('', home),  # 👈 ADICIONA ISSO
